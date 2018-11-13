7c6cfb84779e85a3ce51fedab3551f0a9382ddda
leonardofurlan <leonardofurlan@michaelpage.com.au>
2018-11-13 11:41:51 +1100

    Adding the project to the GitHub Repo

---
+++ HelloWorld/src/com/LeosTests/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.LeosTests.HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World Leo!");
+		System.out.println("Hello World second time!");
+		System.out.println("Hello World third time!");
+		
+	}
+
+}
---
+++ HelloWorld/src/com/LeosTests/HelloWorld/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author leonardofurlan
+ *
+ */
+package com.LeosTests.HelloWorld;
